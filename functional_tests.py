0eb0cc4fc36c17bfd4dfc342cb524eda0473837c
Victor Ciurana <ciurana2016@gmail.com>
2018-09-12 21:08:07 +0200

    more

--- functional_tests.py
+++ functional_tests.py
@@ -39,20 +39,25 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy vegan bacon' for row in rows),
-            'New to-do item did not appear in table'
-        )
+        self.assertIn('1: Buy vegan bacon' , [row.text for row in rows])
 
         # There is still a text box inviting her to add another item.
         # She enteres "Cook that vegan bacon"
-        self.fail('Finish the test!s')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Cook that vegan bacon')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy vegan bacon' , [row.text for row in rows])
+        self.assertIn('2: Cook that vegan bacon' , [row.text for row in rows])
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
+        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
