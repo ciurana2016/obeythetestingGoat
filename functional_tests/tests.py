8315211925d759e6e286b8b4c2b6a1376cb5a381
Victor Ciurana <ciurana2016@gmail.com>
2018-09-24 11:44:20 +0200

    first FT for CSS WOW!

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -15,6 +15,31 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def test_layout_and_styling(self):
+        # Edith foes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10 #letting us specify that we want our arithmetic to work to within plus or minus 10 pixels.
+        )
+
+        # She starts a new list and sees the input is nicely centered here too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+        
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
