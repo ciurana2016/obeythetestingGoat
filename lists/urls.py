c868f445d3867e3276d34b93c9df5c8d207afab7
Victor Ciurana <ciurana2016@gmail.com>
2018-09-24 11:25:34 +0200

    urls go to the app folder

---
+++ lists/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from lists import views
+
+urlpatterns = [
+    path('new', views.new_list, name='new_list'),
+    path('<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/add_item', views.add_item, name='add_item'),
+]
