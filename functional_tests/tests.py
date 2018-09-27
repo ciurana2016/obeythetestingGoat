1a4ea380edca2d4c330fce6ea4ff78c972bd530a
Victor Ciurana <ciurana2016@gmail.com>
2018-09-27 13:27:42 +0200

    using bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,12 +2,12 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
